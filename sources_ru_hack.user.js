ab986cfbc890f629e63af68b9c7314392b818ac4
Minin Yuri <J.Jureth@gmail.com>
2012-08-27 21:19:49 +0700

    bug: fixup to previous update

--- sources_ru_hack.user.js
+++ sources_ru_hack.user.js
@@ -2,9 +2,10 @@
 // @name forum_sources_ru_hack
 // @description Хак внешнего вида форума на исходниках
 // @author Yuri 'Jureth' Minin
-// @license Personal use only. Not for distribution
+// @license MIT
 // @version 1.1
 // ==/UserScript==
+
 // [1] Оборачиваем скрипт в замыкание, для кроссбраузерности (opera, ie)
 (function (window, undefined) {
 // [2] нормализуем window
@@ -14,14 +15,72 @@
         return;
     }
 
+    function supports_html5_storage() {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch (e) {
+            return false;
+        }
+    }
+
+		/**
+		 * Retreive ignore list
+		 */
+    function get_excluded_users(){
+        users = localStorage['sources.forum.excluded'];
+        if ( typeof users == 'undefined' ) {
+            return new Array();
+        }
+        users = JSON.parse(users);
+        if ( !$.isArray(users) ) {
+            users = new Array();
+        }
+        return users;
+    }
+
+		/**
+		 * Add user to ignore list
+		 */
+    function add_user_to_excluded(name){
+        users = get_excluded_users();
+        id = $.inArray(name, users);
+        if ( id == -1 ) {
+            users.push(name);
+            console.log(users);
+            localStorage['sources.forum.excluded'] = JSON.stringify(users);
+        }
+    }
+    /**
+		 * Delete user from ignore list
+		 */
+    function remove_user_from_excluded(name){
+        users = get_excluded_users();
+        id = $.inArray(name, users);
+        if ( id != -1 ) {
+            delete users[id];
+            localStorage['sources.forum.excluded'] = JSON.stringify(users);
+        }
+    }
+
+		/**
+		 * Hide posts of ignored users
+		 */
+    function hide_excluded_posts(){
+        var excluded = get_excluded_users();
+        $posts_to_hide = $('.postlinksbar').closest('.tableborder').find('table').filter(function (index) {
+            return -1 != jQuery.inArray($(this).find('.normalname a').first().text(), excluded);
+        });
+        $posts_to_hide.each(function () {
+            $(this).find('tr:eq(1) td:eq(1) div.postcolor').each(function () {
+                $(this).html("<div class='spoiler closed'><div class='spoiler_header'>Не интересно</div><div class='body'>" + $(this).html() + "</div></div>");
+                $(this).find('.spoiler_header').first().click(function () {
+                    openCloseParent(this);
+                });
+            })
+        });
+    }
+
     if (/forum.sources.ru/.test(w.location.href)) {
-        var excluded_users = new Array(
-            'DrUnkard',
-            'Koss',
-            'мыш',
-            '#SI#',
-            'Booze'
-        );
         $.getScript('http://forum.sources.ru/html/global.js');
         $(document).ready(function () {
             //удаление ссылок на правила, поиск и т.д.
@@ -64,21 +123,28 @@
 
             //поиск "неугодных" сообщений
             if (location.href.indexOf('showtopic=') > 0) {
-                $posts_to_hide = $('.postlinksbar').closest('.tableborder').find('table').filter(function (index) {
-                    return -1 != jQuery.inArray($(this).find('.normalname a').first().text(), excluded_users);
+                var excluded = get_excluded_users();
+                hide_excluded_posts();
+                //add links
+                $table = $('.postlinksbar').closest('.tableborder').find('table');
+                $table.each(function(){
+                    $(this).find('tr:eq(1) .postdetails').append('<a class="add_excluded_link">Добавить в игнор</a>');
+                    $(this).find('tr:eq(1) .postdetails').append('<a class="remove_excluded_link">Убрать из игнора</a>');
+                    if ( -1 != jQuery.inArray($(this).find('.normalname a').first().text(), excluded) ) {
+                        $(this).find('.add_excluded_link').hide();
+                    }else{
+                        $(this).find('.remove_excluded_link').hide();
+                    }
                 });
-                $posts_to_hide.each(function () {
-                    $(this).find('tr:eq(1) td:eq(1) div.postcolor').each(function () {
-                        $(this).html("<div class='spoiler closed'><div class='spoiler_header'>Не интересно</div><div class='body'>" + $(this).html() + "</div></div>");
-                        $(this).find('.spoiler_header').first().click(function () {
-                            openCloseParent(this);
-                        });
-                    })
+                $('a.add_excluded_link').click(function (){
+                    add_user_to_excluded($(this).closest('table').find('.normalname a').first().text());
+                    w.location.reload();
+                });
+                $('a.remove_excluded_link').click(function (){
+                    remove_user_from_excluded($(this).closest('table').find('.normalname a').first().text());
+                    w.location.reload();
                 });
             }
-        if ( supports_html5_storage() ) {
-		alert('ye');
-	}
         })
     }
 })(window);
