75457c6c0b512bd597ae2f7bd4dba1334ae37b5f
Minin Yuri <J.Jureth@gmail.com>
2012-08-27 17:51:14 +0700

    add: userscript behavior

--- script.js
+++ script.js
@@ -1,58 +1,85 @@
-if ( location.href.indexOf('forum.sources.ru') > 0 ){
-    var excluded_users = new Array(
-        'DrUnkard',
-        'Koss',
-        'мыш',
-        '#SI#',
-        'Booze'
-    );
-    $(document).ready(function(){
-        //удаление новостей
-        $news = $('#navstrip').closest('table').next().next();
-        if ( $news.html().indexOf('новости') > -1 || $news.html().indexOf('голосования') > -1 ) {
-            $news.remove();
-        }
-        //замена моего помошника на избранное
-        $buddy = $('a[href*="buddy_pop()"]');
-        $buddy.text('Избранное');
-        $buddy.attr('href', "http://forum.sources.ru/index.php?act=fav&show=1");
-        $buddy.attr('id', 'buddy');
-        $buddy.attr('title', '');
-        if ( $('table#submenu').find('img[src*="atb_favs_new.gif"]').length > 0 ) {
-            //реакция на наличие обновлений избранных тем
-            $buddy.css('color', 'red');
-        }
-        //удаление баннеров и дай5
-        $('#navstrip').siblings().text('');
-        //удаление ссылок на правила, поиск и т.д.
-        $('#submenu').remove();
-        //удаление шапки
-        $('#logostrip').closest('table').remove();
+// ==UserScript==
+// @name forum_sources_ru_hack
+// @description Хак внешнего вида форума на исходниках
+// @author Yuri 'Jureth' Minin
+// @license MIT
+// @version 1.0
+// ==/UserScript==
+// [1] Оборачиваем скрипт в замыкание, для кроссбраузерности (opera, ie)
+(function (window, undefined) {
+// [2] нормализуем window
+    var w;
+    if (typeof unsafeWindow != undefined) {
+        w = unsafeWindow
+    } else {
+        w = window;
+    }
+    if (w.self != w.top) {
+        return;
+    }
 
-        //отступ от верхнего края
-        $('#userlinks').css('margin-top', '10px');
+    if (/forum.sources.ru/.test(w.location.href)) {
+        var excluded_users = new Array(
+            'DrUnkard',
+            'Koss',
+            'мыш',
+            '#SI#',
+            'Booze'
+        );
+        $(document).ready(function () {
+            //удаление новостей
+            $news = $('#navstrip').closest('table').next().next();
+            if ($news.html().indexOf('новости') > -1 || $news.html().indexOf('голосования') > -1) {
+                $news.remove();
+            }
+            //замена моего помошника на избранное
+            $buddy = $('a[href*="buddy_pop()"]');
+            $buddy.text('Избранное');
+            $buddy.attr('href', "http://forum.sources.ru/index.php?act=fav&show=1");
+            $buddy.attr('id', 'buddy');
+            $buddy.attr('title', '');
+            if ($('table#submenu').find('img[src*="atb_favs_new.gif"]').length > 0) {
+                //реакция на наличие обновлений избранных тем
+                $buddy.css('color', 'red');
+            }
+            //удаление баннеров и дай5
+            $('#navstrip').siblings().text('');
+            //удаление ссылок на правила, поиск и т.д.
+            $('#submenu').remove();
+            //удаление шапки
+            $('#logostrip').closest('table').remove();
 
-        //удаление статистики форума
-        $('div:contains("Статистика форума")').closest('.tableborder').remove();
-        //удаление powered by invision и прочей информации
-        $('#ipbwrapper').children('noindex').remove();
-        $('#ipbwrapper').children('div').not('.tableborder,.tablefill,#qr_open,#ipbwrapper').remove();
+            //отступ от верхнего края
+            $('#userlinks').css('margin-top', '10px');
 
-        //обёртывание правил в спойлер
-        $('#ipbwrapper #ipbwrapper').before("<div class='postcolor' id='rules_spoiler'><div class='spoiler closed'><div class='spoiler_header'>Правила</div><div class='body'></div></div><div>");
-        $('#rules_spoiler .spoiler_header').click(function(){openCloseParent(this);});
-        $('#rules_spoiler .body').append($('#ipbwrapper #ipbwrapper'));
+            //удаление статистики форума
+            $('div:contains("Статистика форума")').closest('.tableborder').remove();
+            //удаление powered by invision и прочей информации
+            $('#ipbwrapper').children('noindex').remove();
+            $('#ipbwrapper').children('div').not('.tableborder,.tablefill,#qr_open,#ipbwrapper').remove();
 
-        //поиск "неугодных" сообщений
-        if ( location.href.indexOf('showtopic=') > 0 ){
-            $posts_to_hide = $('.postlinksbar').closest('.tableborder').find('table').filter(function(index){ return -1 != jQuery.inArray($(this).find('.normalname a').first().text(), excluded_users); });
-            $posts_to_hide.each(function(){ $(this).find('tr:eq(1) td:eq(1) div.postcolor').each(function(){
-                $(this).html("<div class='spoiler closed'><div class='spoiler_header'>Не интересно</div><div class='body'>" + $(this).html() + "</div></div>");
-                $(this).find('.spoiler_header').first().click(function(){openCloseParent(this);});
-            })});
-        }
-    })
-}
+            //обёртывание правил в спойлер
+            $('#ipbwrapper #ipbwrapper').before("<div class='postcolor' id='rules_spoiler'><div class='spoiler closed'><div class='spoiler_header'>Правила</div><div class='body'></div></div><div>");
+            $('#rules_spoiler .spoiler_header').click(function () {
+                openCloseParent(this);
+            });
+            $('#rules_spoiler .body').append($('#ipbwrapper #ipbwrapper'));
 
-// vim:expandtab:ts=4:sts=4
+            //поиск "неугодных" сообщений
+            if (location.href.indexOf('showtopic=') > 0) {
+                $posts_to_hide = $('.postlinksbar').closest('.tableborder').find('table').filter(function (index) {
+                    return -1 != jQuery.inArray($(this).find('.normalname a').first().text(), excluded_users);
+                });
+                $posts_to_hide.each(function () {
+                    $(this).find('tr:eq(1) td:eq(1) div.postcolor').each(function () {
+                        $(this).html("<div class='spoiler closed'><div class='spoiler_header'>Не интересно</div><div class='body'>" + $(this).html() + "</div></div>");
+                        $(this).find('.spoiler_header').first().click(function () {
+                            openCloseParent(this);
+                        });
+                    })
+                });
+            }
+        })
+    }
+})
 
