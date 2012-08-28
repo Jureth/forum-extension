0add0685dd00746d6587a0399fbe2869aff9ac11
Minin Yuri <J.Jureth@gmail.com>
2012-08-28 21:54:24 +0700

    bug: some bugs

--- sources_ru_hack.user.js
+++ sources_ru_hack.user.js
@@ -62,78 +62,77 @@
         }
     }
 
-    if (/^http:\/\/forum.sources.ru/.test(w.location.href)) {
-        $.noConflict();
-        jQuery.getScript('http://forum.sources.ru/html/global.js');
-        jQuery(document).ready(function ($) {
-            //удаление ссылок на правила, поиск и т.д.
-            //удаление новостей
-            $news = $('#navstrip').closest('table').next().next();
-            if ($news.html().indexOf('новости') > -1 || $news.html().indexOf('голосования') > -1) {
-                $news.hide();
-            }
-            //замена моего помошника на избранное
-            $buddy = $('a[href*="buddy_pop()"]');
-            $buddy.text('Избранное');
-            $buddy.attr('href', "http://forum.sources.ru/index.php?act=fav&show=1");
-            $buddy.attr('id', 'buddy');
-            $buddy.attr('title', '');
-            if ($('table#submenu').find('img[src*="atb_favs_new.gif"]').length > 0) {
-                //реакция на наличие обновлений избранных тем
-                $buddy.css('color', 'red');
-            }
+    $.noConflict();
+    jQuery.getScript('http://forum.sources.ru/html/global.js');
+    jQuery(document).ready(function ($) {
+        //удаление ссылок на правила, поиск и т.д.
 
-            //удаление баннеров и дай5
-            //удаление статистики форума
-            $('div:contains("Статистика форума")').closest('.tableborder').hide();
+        $('#submenu').first().hide();
+        //удаление новостей
+        $news = $('#navstrip').closest('table').next().next();
+        if ( $news.attr('id') == 'submenu'){
+            $news = $news.next();
+        }
+        if ($news.html().indexOf('новости') > -1 || $news.html().indexOf('голосования') > -1) {
+            $news.hide();
+        }
+        //замена моего помошника на избранное
+        $('a[href*="buddy_pop()"]').replaceWith('<a href="http://forum.sources.ru/index.php?act=fav&show=1" id="new_favorites_button">Избранное</a>')
+        if ($('table#submenu:first').find('img[src*="atb_favs_new.gif"]').length > 0) {
+            //реакция на наличие обновлений избранных тем
+            $('#new_favorites_button').css('color', 'red');
+        }
 
-            //удаление powered by invision и прочей информации
-            //не реализуется через css. поведение :NOT отличается
-            $('#ipbwrapper > div:not(".tableborder,.tablefill,#qr_open,#ipbwrapper")').hide();
-            //обёртывание правил в спойлер
-            $('#ipbwrapper #ipbwrapper').before("<div class='postcolor' id='rules_spoiler'><div class='spoiler closed'><div class='spoiler_header'>Правила</div><div class='body'></div></div><div>");
-            $('#rules_spoiler .spoiler_header').click(function () {
-                openCloseParent(this);
+        //удаление статистики форума
+        $('div:contains("Статистика форума")').closest('.tableborder').hide();
+
+        //удаление powered by invision и прочей информации
+        //не реализуется через css. поведение :NOT отличается
+        $('#ipbwrapper > div:not(".tableborder,.tablefill,#qr_open,#ipbwrapper")').hide();
+
+        //обёртывание правил в спойлер
+        $('#ipbwrapper #ipbwrapper').before("<div class='postcolor' id='rules_spoiler'><div class='spoiler closed'><div class='spoiler_header'>Правила</div><div class='body'></div></div><div>");
+        $('#rules_spoiler .spoiler_header').click(function () {
+            openCloseParent(this);
+        });
+        $('#rules_spoiler .body').append($('#ipbwrapper #ipbwrapper'));
+
+        //поиск "неугодных" сообщений
+        if (location.href.indexOf('showtopic=') > 0) {
+            var excluded = excluded_users.get();
+            $posts_to_hide = $('.postlinksbar').closest('.tableborder').find('table').filter(function (index) {
+                return -1 != jQuery.inArray($(this).find('.normalname a:first').text(), excluded);
             });
-            $('#rules_spoiler .body').append($('#ipbwrapper #ipbwrapper'));
+            $posts_to_hide.each(function () {
+                $(this).find('tr:eq(1) td:eq(1) div.postcolor').each(function () {
+                    $(this).html("<div class='spoiler closed'><div class='spoiler_header'>Не интересно</div><div class='body'>" + $(this).html() + "</div></div>");
+                    $(this).find('.spoiler_header:first').click(function () {
+                        openCloseParent(this);
+                    });
+                })
+            });
 
-            //поиск "неугодных" сообщений
-            if (location.href.indexOf('showtopic=') > 0) {
-                var excluded = excluded_users.get();
-                $posts_to_hide = $('.postlinksbar').closest('.tableborder').find('table').filter(function (index) {
-                    return -1 != jQuery.inArray($(this).find('.normalname a:first').text(), excluded);
-                });
-                $posts_to_hide.each(function () {
-                    $(this).find('tr:eq(1) td:eq(1) div.postcolor').each(function () {
-                        $(this).html("<div class='spoiler closed'><div class='spoiler_header'>Не интересно</div><div class='body'>" + $(this).html() + "</div></div>");
-                        $(this).find('.spoiler_header:first').click(function () {
-                            openCloseParent(this);
-                        });
-                    })
-                });
+            //add links
+            $table = $('.postlinksbar').closest('.tableborder').find('table');
+            $table.each(function(){
+                $(this).find('tr:eq(1) .postdetails').append('<a href="#" class="add_excluded_link">Добавить в игнор</a>');
+                $(this).find('tr:eq(1) .postdetails').append('<a href="#" class="remove_excluded_link">Убрать из игнора</a>');
+                if ( -1 != jQuery.inArray($(this).find('.normalname a:first').text(), excluded) ) {
+                    $(this).find('.add_excluded_link').hide();
+                }else{
+                    $(this).find('.remove_excluded_link').hide();
+                }
+            });
 
-                //add links
-                $table = $('.postlinksbar').closest('.tableborder').find('table');
-                $table.each(function(){
-                    $(this).find('tr:eq(1) .postdetails').append('<a href="#" class="add_excluded_link">Добавить в игнор</a>');
-                    $(this).find('tr:eq(1) .postdetails').append('<a href="#" class="remove_excluded_link">Убрать из игнора</a>');
-                    if ( -1 != jQuery.inArray($(this).find('.normalname a:first').text(), excluded) ) {
-                        $(this).find('.add_excluded_link').hide();
-                    }else{
-                        $(this).find('.remove_excluded_link').hide();
-                    }
-                });
-
-                $('a.add_excluded_link').click(function (){
-                    excluded_users.add($(this).closest('table').find('.normalname a').first().text());
-                    w.location.reload();
-                });
-                $('a.remove_excluded_link').click(function (){
-                    excluded_users.remove($(this).closest('table').find('.normalname a').first().text());
-                    w.location.reload();
-                });
-            }
-        })
-    }
+            $('a.add_excluded_link').click(function (){
+                excluded_users.add($(this).closest('table').find('.normalname a').first().text());
+                w.location.reload();
+            });
+            $('a.remove_excluded_link').click(function (){
+                excluded_users.remove($(this).closest('table').find('.normalname a').first().text());
+                w.location.reload();
+            });
+        }
+    })
 })(window);
 
