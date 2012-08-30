37c894d72df203a80544f780c926e5be8919cd41
Minin Yuri <J.Jureth@gmail.com>
2012-08-30 22:42:50 +0700

    upd: some refactor

--- sources_ru_hack.user.js
+++ sources_ru_hack.user.js
@@ -2,7 +2,7 @@
 // @name forum_sources_ru_hack
 // @description Хак внешнего вида форума на исходниках
 // @author Yuri 'Jureth' Minin
-// @license MIT
+// @license Personal use only.
 // @version 1.1
 // ==/UserScript==
 
@@ -63,19 +63,20 @@
     }
 
     $.noConflict();
-    jQuery.getScript('http://forum.sources.ru/html/global.js');
     jQuery(document).ready(function ($) {
         //удаление ссылок на правила, поиск и т.д.
 
-        $('#submenu').first().hide();
-        //удаление новостей
+        $('#submenu').first().remove();
+        //удаление новостей. Пока костылями
         $news = $('#navstrip').closest('table').next().next();
         if ( $news.attr('id') == 'submenu'){
             $news = $news.next();
         }
         if ($news.html().indexOf('новости') > -1 || $news.html().indexOf('голосования') > -1) {
-            $news.hide();
+            $news.remove();
         }
+        delete $news;
+
         //замена моего помошника на избранное
         $('a[href*="buddy_pop()"]').replaceWith('<a href="http://forum.sources.ru/index.php?act=fav&show=1" id="new_favorites_button">Избранное</a>')
         if ($('table#submenu:first').find('img[src*="atb_favs_new.gif"]').length > 0) {
@@ -84,53 +85,82 @@
         }
 
         //удаление статистики форума
-        $('div:contains("Статистика форума")').closest('.tableborder').hide();
+        $('div:contains("Статистика форума")').closest('.tableborder').remove();
 
         //удаление powered by invision и прочей информации
         //не реализуется через css. поведение :NOT отличается
-        $('#ipbwrapper > div:not(".tableborder,.tablefill,#qr_open,#ipbwrapper")').hide();
+        $('#ipbwrapper > div:not(".tableborder,.tablefill,#qr_open,#ipbwrapper")').remove();
 
-        //обёртывание правил в спойлер
-        $('#ipbwrapper #ipbwrapper').before("<div class='postcolor' id='rules_spoiler'><div class='spoiler closed'><div class='spoiler_header'>Правила</div><div class='body'></div></div><div>");
-        $('#rules_spoiler .spoiler_header').click(function () {
-            openCloseParent(this);
-        });
-        $('#rules_spoiler .body').append($('#ipbwrapper #ipbwrapper'));
+        //Скрытие правил
+        var $rules = $('#ipbwrapper #ipbwrapper');
+//        $rules.hide();
+        $('<span id="rules_spoiler">Правила</span>').insertBefore($rules)
+            .css({'cursor': 'pointer', 'color': 'red', 'font-style': 'bold'})
+            .toggle(function(){ $rules.show('fast')}, function(){$rules.hide('fast')});
+        delete rules;
 
         //поиск "неугодных" сообщений
-        if (location.href.indexOf('showtopic=') > 0) {
+        if (/showtopic=/.test(w.location.href)) {
             var excluded = excluded_users.get();
-            $posts_to_hide = $('.postlinksbar').closest('.tableborder').find('table').filter(function (index) {
-                return -1 != jQuery.inArray($(this).find('.normalname a:first').text(), excluded);
-            });
-            $posts_to_hide.each(function () {
-                $(this).find('tr:eq(1) td:eq(1) div.postcolor').each(function () {
-                    $(this).html("<div class='spoiler closed'><div class='spoiler_header'>Не интересно</div><div class='body'>" + $(this).html() + "</div></div>");
-                    $(this).find('.spoiler_header:first').click(function () {
-                        openCloseParent(this);
-                    });
-                })
-            });
 
             //add links
             $table = $('.postlinksbar').closest('.tableborder').find('table');
             $table.each(function(){
-                $(this).find('tr:eq(1) .postdetails').append('<a href="#" class="add_excluded_link">Добавить в игнор</a>');
-                $(this).find('tr:eq(1) .postdetails').append('<a href="#" class="remove_excluded_link">Убрать из игнора</a>');
-                if ( -1 != jQuery.inArray($(this).find('.normalname a:first').text(), excluded) ) {
-                    $(this).find('.add_excluded_link').hide();
-                }else{
-                    $(this).find('.remove_excluded_link').hide();
-                }
-            });
+                if ( -1 != $.inArray($(this).find('.normalname a:first').text(), excluded) ) {
+                    //В списке
+                    //Кнопка работы с игнором
+                    $(this).find('tr:eq(1) .postdetails').append(
+                        $('<a>', {
+                            href:"#",
+                            class:"remove_excluded_link",
+                            text:"Убрать из игнора",
+                            click: function (){
+                                excluded_users.remove($(this).closest('table').find('.normalname a:first').text());
+                                w.location.reload();//самый простой путь.
+                            }
+                        })
+                    );
+                    //Текст поста
+                    var $post = $(this).find('tr:eq(1) div.postcolor:first');
+                    //Переключатель скрыть/показать сообщение
+                    $('<a>', {
+                        href: '#',//иначе нет подчеркивания
+                        class:'j_show_hide_post',
+                        text:'Показать',
+                    })
+                    .toggle(
+                        function(){//show
+                            $(this).text('Скрыть');
+                            $post.show('fast');
+                            $post.siblings('.hide_message').hide();
+                        },
+                        function(){//hide
+                            $(this).text('Показать');
+                            $post.hide('fast');
+                            $post.siblings('.hide_message').show();
+                        }
+                    )
+                    .prependTo($(this).find('tr:eq(0) td:eq(1) div:eq(1)'))
+                    .after(' &middot; ');
+                    //Добавляем сообщение о скрытии
+                    $post.before('<div class="hide_message">Сообщение скрыто</div>')
+                    .hide();
 
-            $('a.add_excluded_link').click(function (){
-                excluded_users.add($(this).closest('table').find('.normalname a').first().text());
-                w.location.reload();
-            });
-            $('a.remove_excluded_link').click(function (){
-                excluded_users.remove($(this).closest('table').find('.normalname a').first().text());
-                w.location.reload();
+                }else{
+                    //Просто добавляем кнопку работы с игнором
+                    $(this).find('tr:eq(1) .postdetails').append(
+                        $('<a>', {
+                            href:"#",
+                            class:"add_excluded_link",
+                            text:"Добавить в игнор",
+                            click:function (){
+                                excluded_users.add($(this).closest('table').find('.normalname a:first').text());
+                                w.location.reload();
+                            }
+                        })
+                    );
+                }
+
             });
         }
     })
