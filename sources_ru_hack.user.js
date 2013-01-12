3ca6d7fd91883fd8eb06816e3053fe3aa95d1ce2
Minin Yuri <J.Jureth@gmail.com>
2013-01-12 11:56:53 +0700

    add: hide topic buttons

--- sources_ru_hack.user.js
+++ sources_ru_hack.user.js
@@ -26,6 +26,7 @@
     });
 
     $.noConflict();
+
     jQuery(document).ready(function ($) {
         //удаление ссылок на правила, поиск и т.д.
 
@@ -42,14 +43,9 @@
             delete $news;
         }
 
-        //замена моего помошника на избранное
-        $('a[href*="buddy_pop()"]').replaceWith('<a href="http://forum.sources.ru/index.php?act=fav&show=1" id="new_favorites_button">Избранное</a>')
-        if ($('table#submenu:first').find('img[src*="atb_favs_new.gif"]').length > 0) {
-            //реакция на наличие обновлений избранных тем
-            $('#new_favorites_button').addClass('have_new');
-        }
+        favButton();
+        clubButton();
 
-	$('#new_favorites_button').after(' &middot; <a href="http://forum.sources.ru/index.php?c=9" id="club_button">Клуб</a>');
         $('#submenu').first().remove();
 
         //удаление статистики форума
@@ -59,121 +55,48 @@
         //не реализуется через css. поведение :NOT отличается
         $('#ipbwrapper > div:not(".tableborder,.tablefill,#qr_open,#ipbwrapper")').remove();
 
-        //Скрытие правил
-        var $rules = $('#ipbwrapper #ipbwrapper');
-//        $rules.hide();
-        $('<span id="rules_spoiler">Правила</span>').insertBefore($rules)
-            .css({'cursor': 'pointer', 'color': 'red', 'font-style': 'bold'})
-            .toggle(function(){ $rules.show('fast')}, function(){$rules.hide('fast')});
-        delete rules;
+        hideRules();
 
         //поиск "неугодных" сообщений
         if (/showtopic=/.test(w.location.href)) {
-            var excluded = excluded_users.get();
-
-            //add links
-            $table = $('.postlinksbar').closest('.tableborder').find('table');
-            $table.each(function(){
-                if ( -1 != $.inArray($(this).find('.normalname a:first').text(), excluded) ) {
-                    //В списке
-                    //Кнопка работы с игнором
-                    $(this).find('tr:eq(1) .postdetails').append(
-                        $('<a>', {
-                            href:"#",
-                            class:"remove_excluded_link",
-                            text:"Убрать из игнора",
-                            click: function (){
-                                excluded_users.remove($(this).closest('table').find('.normalname a:first').text());
-                                w.location.reload();//самый простой путь.
-                            }
-                        })
-                    );
-                    //Текст поста
-                    var $post = $(this).find('tr:eq(1) div.postcolor:first');
-                    //Переключатель скрыть/показать сообщение
-                    $('<a>', {
-                        href: '#',//иначе нет подчеркивания
-                        class:'j_show_hide_post',
-                        text:'Показать',
-                    })
-                    .toggle(
-                        function(){//show
-                            $(this).text('Скрыть');
-                            $post.show('fast');
-                            $post.siblings('.hide_message').hide();
-                        },
-                        function(){//hide
-                            $(this).text('Показать');
-                            $post.hide('fast');
-                            $post.siblings('.hide_message').show();
-                        }
-                    )
-                    .prependTo($(this).find('tr:eq(0) td:eq(1) div:eq(1)'))
-                    .after(' &middot; ');
-                    //Добавляем сообщение о скрытии
-                    $post.before('<div class="hide_message">Сообщение скрыто</div>')
-                    .hide();
-
-                }else{
-                    //Просто добавляем кнопку работы с игнором
-                    $(this).find('tr:eq(1) .postdetails').append(
-                        $('<a>', {
-                            href:"#",
-                            class:"add_excluded_link",
-                            text:"Добавить в игнор",
-                            click:function (){
-                                excluded_users.add($(this).closest('table').find('.normalname a:first').text());
-                                w.location.reload();
-                            }
-                        })
-                    );
-                }
-
-            });
+            hideUserPosts();
+        }else if (/showforum=/.test(w.location.href)){
+            hideTopics();
+        }else if ( /CODE_MODE=(my)?getnew/.test(w.location.href) ){
+            hideSearchResults();
         }
-        else if ( /CODE_MODE=(my)?getnew/.test(w.location.href) ){
-            var excluded = grayed_themes.get();
-            //console.log(excluded);
-            var bgcolor = $(this).find('.tablebasic tr:eq(2) td:first').css('background-color');
-            $('.tablebasic tr:not(:first)').each(function(){
-                var $row = $(this);
-                var themeId = /showtopic=(\d+)/.exec($row.html())[1];
-
-                if ( excluded.indexOf(themeId) > -1 ){
-                    $row.addClass('ignored_theme');
-                }
-
-                $row.find('td:first span').html($('<a>', {
-                    class: 'ignore_theme_button',
-                    html: '<b>#</b>',
-                    css: {
-                        cursor: 'pointer',
-                    },
-                    click: function(){
-                        index = excluded.indexOf(themeId);
-                        if ( index > -1 ){
-//                            console.log('remove ' + themeId);
-                            grayed_themes.remove(themeId);
-                            excluded.remove(index);
-                            $row.removeClass('ignored_theme');
-                        }else{
-//                            console.log('add ' + themeId);
-                            grayed_themes.add(themeId);
-                            excluded.push(themeId);
-                            $row.addClass('ignored_theme');
-                        }
-                        console.log(excluded);
-                    }
-                }));
-
-            })
-        }
+        var bgcolor = jQuery(this).find('.tablebasic tr:eq(2) td:first').css('background-color');
         $('.tablebasic').css('background-color', bgcolor);
+
         if ( global_options['show_create_post_btn']) {
             add_topic_buttons();
         }
     })//ready
 
+    //Скрытие правил
+    function hideRules(){
+        var $rules = jQuery('#ipbwrapper #ipbwrapper');
+        jQuery('<span id="rules_spoiler">Правила</span>').insertBefore($rules)
+            .css({'cursor': 'pointer', 'color': 'red', 'font-style': 'bold'})
+            .toggle(function(){ $rules.show('fast')}, function(){$rules.hide('fast')});
+        delete rules;
+    }
+
+    //Добавление кнопки клуба
+    function clubButton(){
+        jQuery('#new_favorites_button').after(' &middot; <a href="http://forum.sources.ru/index.php?c=9" id="club_button">Клуб</a>');
+    }
+
+    //замена моего помошника на избранное
+    function favButton(){
+        jQuery('a[href*="buddy_pop()"]').replaceWith('<a href="http://forum.sources.ru/index.php?act=fav&show=1" id="new_favorites_button">Избранное</a>')
+        if (jQuery('table#submenu:first').find('img[src*="atb_favs_new.gif"]').length > 0) {
+            //реакция на наличие обновлений избранных тем
+            jQuery('#new_favorites_button').addClass('have_new');
+        }
+    }
+
+    //Добавление кнопок создания темы/опроса
     function add_topic_buttons(){
         //add new poll && new topic buttons
         jQuery('.row2 b a[href*="showforum"]').each(function()
@@ -186,5 +109,154 @@
                  jQuery(links).prependTo(jQuery(this).closest('td'));
             }
         });
+    }
+
+    function hideUserPosts(){
+        var excluded = excluded_users.get();
+
+        //add links
+        $table = jQuery('.postlinksbar').closest('.tableborder').find('table');
+        $table.each(function(){
+            if ( -1 != jQuery.inArray(jQuery(this).find('.normalname a:first').text(), excluded) ) {
+                //В списке
+                //Кнопка работы с игнором
+                jQuery(this).find('tr:eq(1) .postdetails').append(
+                    jQuery('<a>', {
+                        href:"#",
+                        class:"remove_excluded_link",
+                        text:"Убрать из игнора",
+                        click: function (){
+                            excluded_users.remove(jQuery(this).closest('table').find('.normalname a:first').text());
+                            w.location.reload();//самый простой путь.
+                        }
+                    })
+                );
+                //Текст поста
+                var $post = jQuery(this).find('tr:eq(1) div.postcolor:first');
+                //Переключатель скрыть/показать сообщение
+                jQuery('<a>', {
+                    href: '#',//иначе нет подчеркивания
+                    class:'j_show_hide_post',
+                    text:'Показать',
+                })
+                .toggle(
+                    function(){//show
+                        jQuery(this).text('Скрыть');
+                        $post.show('fast');
+                        $post.siblings('.hide_message').hide();
+                    },
+                    function(){//hide
+                        jQuery(this).text('Показать');
+                        $post.hide('fast');
+                        $post.siblings('.hide_message').show();
+                    }
+                )
+                .prependTo(jQuery(this).find('tr:eq(0) td:eq(1) div:eq(1)'))
+                .after(' &middot; ');
+                //Добавляем сообщение о скрытии
+                $post.before('<div class="hide_message">Сообщение скрыто</div>')
+                .hide();
+
+            }else{
+                //Просто добавляем кнопку работы с игнором
+                jQuery(this).find('tr:eq(1) .postdetails').append(
+                    jQuery('<a>', {
+                        href:"#",
+                        class:"add_excluded_link",
+                        text:"Добавить в игнор",
+                        click:function (){
+                            excluded_users.add(jQuery(this).closest('table').find('.normalname a:first').text());
+                            w.location.reload();
+                        }
+                    })
+                );
+            }
+
+        });
+
+    }
+
+    function hideSearchResults(){
+        var excluded = grayed_themes.get();
+        //console.log(excluded);
+        var bgcolor = jQuery(this).find('.tablebasic tr:eq(2) td:first').css('background-color');
+        jQuery('.tablebasic tr:not(:first)').each(function(){
+            var $row = jQuery(this);
+            var themeId = /showtopic=(\d+)/.exec($row.html())[1];
+
+            if ( excluded.indexOf(themeId) > -1 ){
+                $row.addClass('ignored_topic');
+            }
+
+            $row.find('td:first span').html(jQuery('<a>', {
+                class: 'ignore_theme_button',
+                html: '<b>#</b>',
+                css: {
+                    cursor: 'pointer',
+                },
+                click: function(){
+                    index = excluded.indexOf(themeId);
+                    if ( index > -1 ){
+//                            console.log('remove ' + themeId);
+                        grayed_themes.remove(themeId);
+                        excluded.remove(index);
+                        $row.removeClass('ignored_topic');
+                    }else{
+//                            console.log('add ' + themeId);
+                        grayed_themes.add(themeId);
+                        excluded.push(themeId);
+                        $row.addClass('ignored_topic');
+                    }
+                    console.log(excluded);
+                }
+            }));
+
+        })
+    }
+
+    function hideTopics(){
+        var excluded = grayed_themes.get();
+        var $add_button = jQuery('<a>', {
+            class: 'show-topic-button',
+            html: 'Показать',
+            click: function(){
+                topicId = jQuery(this).attr('topicId');
+                grayed_themes.remove(topicId);
+                delete excluded[excluded.indexOf(topicId)];
+                jQuery(this).closest('tr').removeClass('ignored_topic');
+                $btn = $del_button.clone(true);
+                $btn.attr('topicId', topicId);
+                jQuery(this).replaceWith($btn);
+            }
+        });
+        var $del_button = jQuery('<a>', {
+            class: 'hide-topic-button',
+            html: 'Скрыть',
+            click: function(){
+                topicId = jQuery(this).attr('topicId');
+                grayed_themes.add(topicId);
+                excluded.push(topicId);
+                jQuery(this).closest('tr').addClass('ignored_topic');
+                $btn = $add_button.clone(true);
+                $btn.attr('topicId', topicId);
+                jQuery(this).replaceWith($btn);
+            }
+        });
+
+        jQuery('a[href*=showtopic]').parents('tr').each(function(){
+            var $row = jQuery(this);
+            var topicId = /showtopic=(\d+)/.exec($row.html())[1];
+
+            if ( excluded.indexOf(topicId) > -1 ){
+                $row.addClass('ignored_topic');
+                var $btn = $add_button.clone(true);
+                $btn.attr('topicId', topicId);
+                $row.find('td:eq(2)').prepend($btn);
+            }else{
+                $btn = $del_button.clone(true);
+                $btn.attr('topicId', topicId);
+                $row.find('td:eq(2)').prepend($btn);
+            }
+        })
     }
 })(window);
